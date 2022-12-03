7829101b7d44eab780711a250a7555a7531a5eda
Melvin Vermeer <melvin.vermeer@gmail.com>
2022-12-03 23:20:58 +0100

    tests for day 3

---
+++ test/03-rucksack-reorganization.test.ts
@@ -0,0 +1,35 @@
+import { part1, part2 } from "../src/03-rucksack-reorganization";
+import { readFileSync } from "fs";
+
+const data = readFileSync("test/data/03", "utf8").split("\n");
+
+describe("03 - Rucksack Reorganization", () => {
+  const sample = [
+    "vJrwpWtwJgWrhcsFMMfFFhFp",
+    "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
+    "PmmdzqPrVvPwwTWBwg",
+    "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
+    "ttgJtRGJQctTZtZT",
+    "CrZsJsPPZsGzwwsLwLmpwMDw",
+  ];
+
+  describe("Part 1", () => {
+    it("Sample", () => {
+      expect(part1(sample)).toEqual(157);
+    });
+
+    it("Answer", () => {
+      expect(part1(data)).toBe(8252);
+    });
+  });
+
+  describe("Part 2", () => {
+    it("Sample", () => {
+      expect(part2(sample)).toEqual(70);
+    });
+
+    it("Answer", () => {
+      expect(part2(data)).toEqual(2828);
+    });
+  });
+});
