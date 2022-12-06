84d7ba945f51a3732878fbac51f9a2431750fd8c
Melvin Vermeer <melvin.vermeer@gmail.com>
2022-12-06 12:29:25 +0100

    fetch puzzle input

--- scripts/new-challenge.ts
+++ scripts/new-challenge.ts
@@ -1,29 +1,45 @@
 import fs from "fs";
 import { execSync } from "child_process";
+const chrome = require("chrome-cookies-secure");
 
-const currentDayNumbers = fs
-  .readdirSync("src/")
-  .filter((file) => fs.lstatSync("src/" + file).isFile())
-  .map((filename) => filename.split("-")[0])
-  .map((x) => Number(x));
+async function newChallenge() {
+  const currentDayNumbers = fs
+    .readdirSync("src/")
+    .filter((file) => fs.lstatSync("src/" + file).isFile())
+    .map((filename) => filename.split("-")[0])
+    .map((x) => Number(x));
 
-const nextDay = Math.max(...currentDayNumbers) + 1;
-const title = process.argv[process.argv.length - 1];
-const kebabTitle = title.replace(/ /g, "-").toLowerCase();
-const newNumber = nextDay.toString().padStart(2, "0");
+  const nextDay = Math.max(...currentDayNumbers) + 1;
+  const title = process.argv[process.argv.length - 1];
+  const kebabTitle = title.replace(/ /g, "-").toLowerCase();
+  const newNumber = nextDay.toString().padStart(2, "0");
 
-fs.copyFileSync("test/data/00", `test/data/${newNumber}`);
-fs.copyFileSync("test/00.test.ts", `test/${newNumber}-${kebabTitle}.test.ts`);
-fs.copyFileSync("src/00-template.ts", `src/${newNumber}-${kebabTitle}.ts`);
+  const { session } = await chrome.getCookiesPromised(
+    "https://www.adventofcode.com"
+  );
 
-execSync(
-  `sed -i "" "s!00 - Template!${newNumber} - ${title}!g" ./test/${newNumber}-${kebabTitle}.test.ts`
-);
+  const response = await fetch(
+    `https://adventofcode.com/2022/day/${newNumber}/input`,
+    {
+      headers: { cookie: `session=${session}` },
+    }
+  );
 
-execSync(
-  `sed -i "" "s!00-template!${newNumber}-${kebabTitle}!g" ./test/${newNumber}-${kebabTitle}.test.ts`
-);
+  fs.writeFileSync(`test/data/${newNumber}`, await response.text());
+  fs.copyFileSync("test/00.test.ts", `test/${newNumber}-${kebabTitle}.test.ts`);
+  fs.copyFileSync("src/00-template.ts", `src/${newNumber}-${kebabTitle}.ts`);
 
-execSync(
-  `sed -i "" "s!data/00!data/${newNumber}!g" ./test/${newNumber}-${kebabTitle}.test.ts`
-);
+  execSync(
+    `sed -i "" "s!00 - Template!${newNumber} - ${title}!g" ./test/${newNumber}-${kebabTitle}.test.ts`
+  );
+
+  execSync(
+    `sed -i "" "s!00-template!${newNumber}-${kebabTitle}!g" ./test/${newNumber}-${kebabTitle}.test.ts`
+  );
+
+  execSync(
+    `sed -i "" "s!data/00!data/${newNumber}!g" ./test/${newNumber}-${kebabTitle}.test.ts`
+  );
+}
+
+newChallenge();
