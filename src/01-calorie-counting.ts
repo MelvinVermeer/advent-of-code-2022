ab0a3721483854b7e7f200998ca2840cfae6e2c5
Melvin Vermeer <melvin.vermeer@gmail.com>
2022-12-07 13:20:28 +0100

    clean up

--- src/01-calorie-counting.ts
+++ src/01-calorie-counting.ts
@@ -1,4 +1,5 @@
 import { sum } from "./shared/math";
+import { descending } from "./shared/sorting";
 
 export const part1 = (elves: string[][]): any =>
   Math.max(...elves.map((elf) => elf.map(Number).reduce(sum)));
@@ -6,6 +7,6 @@ export const part1 = (elves: string[][]): any =>
 export const part2 = (elves: string[][]): any =>
   elves
     .map((elf) => elf.map(Number).reduce(sum))
-    .sort((a, b) => b - a)
+    .sort(descending)
     .slice(0, 3)
     .reduce(sum);
