736e16ed5ea0308cae86a0276ba5e1f07056bb43
Melvin Vermeer <melvin.vermeer@gmail.com>
2022-12-12 17:59:21 +0100

    clean

--- src/08-treetop-tree-house.ts
+++ src/08-treetop-tree-house.ts
@@ -1,43 +1,13 @@
-import { rangeToArray } from "./shared/array";
 import { product } from "./shared/math";
 
 type Grid = (number | string)[][];
 
-const isVisible = (grid: Grid, row: number, column: number) => {
-  const height = grid[row][column];
-
-  const east = rangeToArray(`${column + 1}-${grid[0].length - 1}`);
-  const west = rangeToArray(`0-${column - 1}`);
-  const north = rangeToArray(`0-${row - 1}`);
-  const south = rangeToArray(`${row + 1}-${grid.length - 1}`);
-
-  if (
-    column === grid[0].length - 1 ||
-    east.every((eastColumn) => grid[row][eastColumn] < height)
-  ) {
-    return true;
-  }
-
-  if (
-    column === 0 ||
-    west.every((westColumn) => grid[row][westColumn] < height)
-  ) {
-    return true;
-  }
-
-  if (row === 0 || north.every((northRow) => grid[northRow][column] < height)) {
-    return true;
-  }
-
-  if (
-    row === grid.length - 1 ||
-    south.every((southRow) => grid[southRow][column] < height)
-  ) {
-    return true;
-  }
-
-  return false;
-};
+const getTreeLines = (grid: Grid, r: number, c: number) => ({
+  north: grid.slice(0, Math.max(r, 0)).map((row) => row[c]),
+  east: grid[r].slice(c + 1),
+  south: grid.slice(r + 1).map((row) => row[c]),
+  west: grid[r].slice(0, Math.max(c, 0)),
+});
 
 export const part1 = (data: string[]): number => {
   const grid = data.map((row) => row.split("").map(Number));
@@ -45,7 +15,13 @@ export const part1 = (data: string[]): number => {
 
   for (let r = 0; r < grid.length; r++) {
     for (let c = 0; c < grid[r].length; c++) {
-      if (isVisible(grid, r, c)) {
+      const { north, east, south, west } = getTreeLines(grid, r, c);
+      const treeLines = [north, east, south, west]
+        .map((line) => line.filter((tree) => tree >= grid[r][c]))
+        .map((x) => x.length);
+
+      const isVisible = treeLines.some((line) => line === 0);
+      if (isVisible) {
         visibleCount++;
       }
     }
@@ -60,20 +36,18 @@ export const part2 = (data: string[]): number => {
 
   for (let r = 0; r < grid.length; r++) {
     for (let c = 0; c < grid[r].length; c++) {
-      const east = grid[r].slice(c + 1);
-      const west = grid[r].slice(0, Math.max(c, 0)).reverse();
-      const south = grid.slice(r + 1).map((row) => row[c]);
-      const north = grid
-        .slice(0, Math.max(r, 0))
-        .map((row) => row[c])
-        .reverse();
-
-      const treeLines = [north, east, south, west].map((line) => {
-        const blockingTreeIndex = line.findIndex((tree) => tree >= grid[r][c]);
-        return blockingTreeIndex === -1 ? line.length : blockingTreeIndex + 1;
-      });
+      const { north, east, south, west } = getTreeLines(grid, r, c);
+      const treeLines = [north.reverse(), east, south, west.reverse()].map(
+        (line) => {
+          const blockingTreeIndex = line.findIndex(
+            (tree) => tree >= grid[r][c]
+          );
+          return blockingTreeIndex === -1 ? line.length : blockingTreeIndex + 1;
+        }
+      );
       maxScore = Math.max(treeLines.reduce(product), maxScore);
     }
   }
+
   return maxScore;
 };
