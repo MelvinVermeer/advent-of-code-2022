69d9a994bb24547d474df98c3404c8285970688b
Melvin Vermeer <melvin.vermeer@gmail.com>
2022-12-12 17:37:13 +0100

    clean

--- src/08-treetop-tree-house.test.ts
+++ src/08-treetop-tree-house.test.ts
@@ -7,11 +7,11 @@ describe("08 - Treetop Tree House", () => {
   const sample = ["30373", "25512", "65332", "33549", "35390"];
 
   describe("Part 1", () => {
-    xit("Sample", () => {
+    it("Sample", () => {
       expect(part1(sample)).toEqual(21);
     });
 
-    xit("Answer", () => {
+    it("Answer", () => {
       expect(part1(data)).toBe(1849);
     });
   });
--- src/08-treetop-tree-house.ts
+++ src/08-treetop-tree-house.ts
@@ -39,122 +39,40 @@ const isVisible = (grid: Grid, row: number, column: number) => {
   return false;
 };
 
-const calculateScenicScore = (grid: Grid, row: number, column: number) => {
-  // const height = grid[row][column];
-
-  const east = rangeToArray(`${column + 1}-${grid[0].length - 1}`);
-  const west = rangeToArray(`0-${column - 1}`);
-  const north = rangeToArray(`0-${row - 1}`);
-  const south = rangeToArray(`${row + 1}-${grid.length - 1}`);
-  const trees: number[] = [];
-
-  if (row === 3 && column === 2) {
-    console.log({ north, east, south, west });
-  }
-
-  // if (row === 30 && column === 56) {
-  //   console.log("");
-  // }
-  //east
-  if (column !== grid[0].length - 1) {
-    let highest = -1;
-    let count = 0;
-    for (let i = 0; i < east.length; i++) {
-      if (grid[row][east[i]] > highest) {
-        count++;
-        highest = Number(grid[row][east[i]]);
-      }
-    }
-    trees.push(count);
-  }
-
-  // west
-  if (column !== 0) {
-    let highest = -1;
-    let count = 0;
-    for (let i = west.length - 1; i > 0; i--) {
-      if (grid[row][west[i]] > highest) {
-        count++;
-        highest = Number(grid[row][west[i]]);
-      }
-    }
-    trees.push(count);
-  }
-
-  // north
-  if (row !== 0) {
-    let highest = -1;
-    let count = 0;
-    for (let i = north.length - 1; i > 0; i--) {
-      if (grid[north[i]][column] > highest) {
-        count++;
-        highest = Number(grid[north[i]][column]);
-      }
-    }
-    trees.push(count);
-  }
-
-  // south
-  if (row !== grid.length - 1) {
-    let highest = -1;
-    let count = 0;
-    for (let i = 0; i < south.length; i++) {
-      if (grid[south[i]][column] > highest) {
-        count++;
-        highest = Number(grid[south[i]][column]);
-      }
-    }
-    trees.push(count);
-  }
-
-  return trees;
-};
-
 export const part1 = (data: string[]): number => {
   const grid = data.map((row) => row.split("").map(Number));
   let visibleCount = 0;
+
   for (let r = 0; r < grid.length; r++) {
     for (let c = 0; c < grid[r].length; c++) {
-      const element = grid[r][c];
-      const v = isVisible(grid, r, c);
-      if (v) {
+      if (isVisible(grid, r, c)) {
         visibleCount++;
       }
     }
   }
-  // console.log(visibleCount);
-  // printGrid(grid);
+
   return visibleCount;
 };
 
-function getScore(grid: number[][], r: number, c: number) {
-  const east = grid[r].slice(c + 1);
-  const west = grid[r].slice(0, Math.max(c, 0)).reverse();
-  const south = grid.slice(r + 1).map((row) => row[c]);
-  const north = grid
-    .slice(0, Math.max(r, 0))
-    .map((row) => row[c])
-    .reverse();
-
-  const height = grid[r][c];
-
-  const treeLines = [north, east, south, west].map((line) => {
-    const blockingTreeIndex = line.findIndex((tree) => tree >= height);
-    return blockingTreeIndex === -1 ? line.length : blockingTreeIndex + 1;
-  });
-
-  return treeLines.reduce(product);
-}
-
 export const part2 = (data: string[]): number => {
   const grid = data.map((row) => row.split("").map(Number));
-
   let maxScore = 0;
 
   for (let r = 0; r < grid.length; r++) {
     for (let c = 0; c < grid[r].length; c++) {
-      const score = getScore(grid, r, c);
-      maxScore = Math.max(score, maxScore);
+      const east = grid[r].slice(c + 1);
+      const west = grid[r].slice(0, Math.max(c, 0)).reverse();
+      const south = grid.slice(r + 1).map((row) => row[c]);
+      const north = grid
+        .slice(0, Math.max(r, 0))
+        .map((row) => row[c])
+        .reverse();
+
+      const treeLines = [north, east, south, west].map((line) => {
+        const blockingTreeIndex = line.findIndex((tree) => tree >= grid[r][c]);
+        return blockingTreeIndex === -1 ? line.length : blockingTreeIndex + 1;
+      });
+      maxScore = Math.max(treeLines.reduce(product), maxScore);
     }
   }
   return maxScore;
